33f91fecb5dd6c8b2f9a1d31dc29307e1615e35e
Alperen Saffet Yeşil <alperensyesil@gmail.com>
2023-11-25 14:40:04 +0300

    Initial commit

---
+++ ConvertLetter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConvertLetter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string appPath = AppDomain.CurrentDomain.BaseDirectory;
+            string path = $"{appPath}tr/global.ini"; // ini dosyanızın yolunu belirtin
+            string content = File.ReadAllText(path, Encoding.Default);
+
+            content = content.Replace('ş', 's')
+                .Replace('Ş', 'S')
+                .Replace('ğ', 'g')
+                .Replace('Ğ', 'G')
+                .Replace('ç', 'c')
+                .Replace('Ç', 'C')
+                .Replace('ö', 'o')
+                .Replace('Ö', 'O')
+                .Replace('ü', 'u')
+                .Replace('Ü', 'U')
+                .Replace('ı', 'i')
+                .Replace('İ', 'I');
+
+            if (Directory.Exists(appPath + "en"))
+                File.Delete(appPath + "en/global.ini");
+            else
+                Directory.CreateDirectory(appPath + "en");
+            
+            path = $"{appPath}en/global.ini";
+            File.WriteAllText(path, content, Encoding.Default);
+        }
+    }
+}
